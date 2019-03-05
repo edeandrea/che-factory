22db624dfd4c398d23873f8a8b20badd6af46302
Eric Deandrea <eric.deandrea@gmail.com>
2019-03-05 15:00:04 -0500

    Adding some code

---
+++ src/main/java/org/erd/SimpleServlet.java
@@ -0,0 +1,19 @@
+package org.erd;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/hello")
+@SuppressWarnings("serial")
+public class SimpleServlet extends HttpServlet {
+@Override
+   protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+      resp.setContentType("text/plain");
+      resp.getWriter().write("Hello World! Maven Web Project Example.");
+   }
+}
